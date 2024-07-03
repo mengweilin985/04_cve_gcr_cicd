2b5725b031a718902544048e566e8b6d9c003f1b
mengweilin985 <61502637+mengweilin985@users.noreply.github.com>
2024-07-03 11:29:13 +0800

    updae

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def index():
 
     return "OK"
 
-# if __name__ == "__main__":
 #     app.run(debug=True)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT",8080)), debug=True)
