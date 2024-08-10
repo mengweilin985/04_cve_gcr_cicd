6f2993f0a94def1c6cbec4bdba1530e4cf118e6b
mengweilin985 <61502637+mengweilin985@users.noreply.github.com>
2024-08-10 20:59:50 +0800

    Update test_gcr.py

--- test/test_gcr.py
+++ test/test_gcr.py
@@ -1,5 +1,5 @@
 import requests
 
-resp = requests.post("https://github-action-cve-run-nyluz5gvmq-as.a.run.app", files={'file': open('0.jpg', 'rb')})
+resp = requests.post("https://github-action-cve-run-nyluz5gvmq-as.a.run.app", files={'file': open('1.jpg', 'rb')})
 
 print(resp.json())
